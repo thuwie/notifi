ee6a918d1bff2a8ac959d311907beb9f6d37080f
kkonoval <Kirill.Konovalov@t-systems.com>
2021-02-20 19:27:53 +0300

    add server

---
+++ index.js
@@ -0,0 +1,37 @@
+const { createServer } = require('http');
+const TelegramRequest = require('./src/TelegramRequest');
+const PORT = process.env.PORT || 5000
+
+
+function handlePostRequest(request) {
+  if (request.method === 'POST') {
+    return new Promise(((resolve, reject) => {
+      let body = '';
+      request.on('data', chunk => {
+        body += chunk.toString();
+      });
+      request.on('end', () => {
+        return resolve(JSON.parse(body));
+      });
+      request.on('error', (error) => {
+        return reject(error);
+      });
+    }));
+  }
+}
+
+const requestHandler = async (req, res) => {
+  if (req.url === '/favicon.ico') return;
+  const body = await handlePostRequest(req);
+  await new TelegramRequest(body).sendRequest();
+  res.end();
+};
+
+const server = createServer(requestHandler);
+
+server.listen(PORT, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(`server is listening on ${PORT}`);
+});
---
+++ src/TelegramRequest.js
@@ -0,0 +1,62 @@
+const { post } = require('axios');
+
+class TelegramRequest {
+  fields = {
+    kind: 'object_kind',
+    attributes: 'object_attributes',
+  };
+
+  events = {
+    mr: 'merge_request',
+    pipeline: 'pipeline',
+  };
+
+  message = 'Empty message';
+  botId = process.env.BOT_ID;
+  chatId = process.env.CHAT_ID;
+  botUrl = `https://api.telegram.org/bot${this.botId}/sendMessage`;
+
+  constructor(body) {
+    this.body = body;
+  }
+
+  createMergeRequestMessage(body) {
+    let message = 'Merge Request\n';
+    message += `Author: ${body.user.name}\n`;
+    message += `Project: ${body.project.name}\n`;
+    message += `Title: ${body[this.fields.attributes].title}\n`;
+    message += `URL: ${body[this.fields.attributes].url}\n`;
+    message += `State: ${body[this.fields.attributes].state}\n`;
+    return message;
+  }
+
+  createPipelineMessage(body) {
+
+  }
+
+  createPayload() {
+    if (this.body[this.fields.kind] === this.events.mr) {
+      this.message = this.createMergeRequestMessage(this.body);
+    }
+
+    if (this.body[this.fields.kind] === this.events.pipeline) {
+      this.message = this.createPipelineMessage(this.body);
+    }
+  }
+
+  async sendRequest() {
+    this.createPayload();
+    try {
+      await post(this.botUrl, null, {
+        params: {
+          chat_id: this.chatId,
+          text: this.message,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+module.exports = TelegramRequest;
